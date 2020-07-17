a1013eafbb2d223e94b95b112de7a42d613af6fd
jerry <email123333@163.com>
2020-07-17 20:14:55 +0800

    配置文件设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,6 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
   },
   {
     path: '/about',
@@ -17,7 +15,7 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      // return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
   }
 ]
