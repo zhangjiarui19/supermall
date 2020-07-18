9ab8eab8780ae1eb595d23af67a49c735f4f8eef
jerry <email123333@163.com>
2020-07-18 13:55:03 +0800

    静态资源引用-assets需要加~

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -1,18 +1,18 @@
 <template>
   <tab-bar>
     <tab-bar-item path="/home" activeColor="pink">
-      <img slot="item-icon" src="assets/img/tabbar/home.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/home_active.svg" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="pink">
-      <img slot="item-icon" src="assets/img/tabbar/category.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/category_active.svg" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
       <div slot="item-text">分类</div>
     </tab-bar-item>
     <tab-bar-item path="/cart" activeColor="pink">
-      <img slot="item-icon" src="assets/img/tabbar/shopcart.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/shopcart_active.svg" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
       <div slot="item-text">购物车</div>
     </tab-bar-item>
     <tab-bar-item path="/profile" activeColor="deepPink">
